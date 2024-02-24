2df419b4488cba23213f69625212182d2d0a96f9
Sharada Shehan <sharadashehan6@gmail.com>
2024-02-25 00:55:59 +0530

    Db config

--- product-service/src/main/java/com/nebulamart/productservice/config/DynamoDbConfiguration.java
+++ product-service/src/main/java/com/nebulamart/productservice/config/DynamoDbConfiguration.java
@@ -1,9 +1,7 @@
 package com.nebulamart.productservice.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
