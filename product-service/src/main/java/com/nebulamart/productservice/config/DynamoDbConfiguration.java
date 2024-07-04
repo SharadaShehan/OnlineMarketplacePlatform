46c46d2c207f2ad7158766580691814aff145fbb
Sharada Shehan <sharadashehan6@gmail.com>
2024-07-04 09:12:57 +0530

    product, contract table beans

--- product-service/src/main/java/com/nebulamart/productservice/config/DynamoDbConfiguration.java
+++ product-service/src/main/java/com/nebulamart/productservice/config/DynamoDbConfiguration.java
@@ -1,10 +1,15 @@
 package com.nebulamart.productservice.config;
 
+import com.nebulamart.productservice.entity.Contract;
+import com.nebulamart.productservice.entity.Product;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,13 +24,31 @@ public class DynamoDbConfiguration {
     private String secretKey;
 
     @Bean
-    public DynamoDbClient dynamoDbClient() {
+    public DynamoDbTable<Product> productTable() {
         AwsCredentialsProvider awsCredentialsProvider = StaticCredentialsProvider.create(
                 AwsBasicCredentials.create(accessKey, secretKey));
 
-        return DynamoDbClient.builder()
-                .region(Region.US_EAST_1)
-                .credentialsProvider(awsCredentialsProvider)
+        DynamoDbEnhancedClient dynamoDbEnhancedClient = DynamoDbEnhancedClient.builder()
+                .dynamoDbClient(DynamoDbClient.builder()
+                        .region(Region.US_EAST_1)
+                        .credentialsProvider(awsCredentialsProvider)
+                        .build())
                 .build();
+        return dynamoDbEnhancedClient.table("Product", TableSchema.fromBean(Product.class));
     }
+
+    @Bean
+    public DynamoDbTable<Contract> contractTable() {
+        AwsCredentialsProvider awsCredentialsProvider = StaticCredentialsProvider.create(
+                AwsBasicCredentials.create(accessKey, secretKey));
+
+        DynamoDbEnhancedClient dynamoDbEnhancedClient = DynamoDbEnhancedClient.builder()
+                .dynamoDbClient(DynamoDbClient.builder()
+                        .region(Region.US_EAST_1)
+                        .credentialsProvider(awsCredentialsProvider)
+                        .build())
+                .build();
+        return dynamoDbEnhancedClient.table("Contract", TableSchema.fromBean(Contract.class));
+    }
+
 }
