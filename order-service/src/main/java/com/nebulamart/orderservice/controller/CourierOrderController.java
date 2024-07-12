7694abb0aed54e7851f229dbad3e4c18a2df6934
Sharada Shehan <sharadashehan6@gmail.com>
2024-07-13 00:02:33 +0530

    order controllers

---
+++ order-service/src/main/java/com/nebulamart/orderservice/controller/CourierOrderController.java
@@ -0,0 +1,38 @@
+package com.nebulamart.orderservice.controller;
+
+import com.nebulamart.orderservice.service.OrderService;
+import com.nebulamart.orderservice.template.OrderUpdateResponseDTO;
+import jakarta.websocket.server.PathParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/order/courier")
+public class CourierOrderController {
+    private final OrderService orderService;
+
+    @Autowired
+    public CourierOrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PatchMapping("dispatched/{orderId}")
+    public ResponseEntity<OrderUpdateResponseDTO> getDispatchedOrder(@RequestHeader("Authorization") String accessToken, @PathVariable("orderId") String orderId) {
+        ResponseEntity<OrderUpdateResponseDTO> responseEntity = orderService.setOrderDispatched(accessToken, orderId);
+        if (responseEntity == null) {
+            return ResponseEntity.status(400).body(new OrderUpdateResponseDTO(null, "Failed to set order as dispatched"));
+        }
+        return responseEntity;
+    }
+
+    @PatchMapping("delivered/{orderId}")
+    public ResponseEntity<OrderUpdateResponseDTO> getDeliveredOrder(@RequestHeader("Authorization") String accessToken, @PathVariable("orderId") String orderId) {
+        ResponseEntity<OrderUpdateResponseDTO> responseEntity = orderService.setOrderDelivered(accessToken, orderId);
+        if (responseEntity == null) {
+            return ResponseEntity.status(400).body(new OrderUpdateResponseDTO(null, "Failed to set order as delivered"));
+        }
+        return responseEntity;
+    }
+
+}
---
+++ order-service/src/main/java/com/nebulamart/orderservice/controller/SellerOrderController.java
@@ -0,0 +1,31 @@
+package com.nebulamart.orderservice.controller;
+
+import com.nebulamart.orderservice.entity.Order;
+import com.nebulamart.orderservice.service.OrderService;
+import jakarta.websocket.server.PathParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/order/seller")
+public class SellerOrderController {
+    private final OrderService orderService;
+
+    @Autowired
+    public SellerOrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<Order>> getOrders(@RequestHeader("Authorization") String accessToken, @PathParam("productId") String productId) {
+        ResponseEntity<List<Order>> responseEntity = orderService.getOrdersByProduct(accessToken, productId);
+        if (responseEntity == null) {
+            return ResponseEntity.status(400).body(null);
+        }
+        return responseEntity;
+    }
+
+}
