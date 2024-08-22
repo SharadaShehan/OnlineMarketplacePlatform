c6700009b05c2a945d285802d6a41be0fa2de6ae
Sharada Shehan <sharadashehan6@gmail.com>
2024-08-22 18:56:11 +0530

    frontend, pipeline modified to get env variables from code build

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment.prod';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,5 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'frontend';
+  apiUrl = environment.apiUrl;
 }
